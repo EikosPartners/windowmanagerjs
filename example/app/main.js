b98227cbc3a5beb2b149589a837bc73b5e99dd9e
Dillon Uzar <dillonu@gmail.com>
2016-06-23 17:24:22 -0400

    In example disable devtools from opening at start

--- example/app/main.js
+++ example/app/main.js
@@ -27,7 +27,7 @@ function createWindow() {
   });
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
