a51f316d17ad9e8a72792a72c21aa6dc384ae2d9
Dillon Uzar <dillonu@gmail.com>
2017-02-01 22:05:15 -0500

    Release 0.12.5

--- dist/windowmanager.js
+++ dist/windowmanager.js
@@ -127,7 +127,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	 */
 	var windowmanager = new _index.EventHandler(windowmanagerEventNames);
 	
-	windowmanager.version = ("0.12.4");
+	windowmanager.version = ("0.12.5");
 	// runtime is set in the respective runtime
 	windowmanager.runtime = {
 	    name: undefined,
